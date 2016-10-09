c9d0c98a800cd5fddf76ac387f73d0eaa658d2d4
ameerhamza6733 <ameerhamza6733@gmail.com>
2016-10-09 14:03:45 +0500

    Using FirebaseUI to Populate a ListView

--- app/src/main/java/com/example/ameerhamza/firebabseyoutubetest/MainActivity.java
+++ app/src/main/java/com/example/ameerhamza/firebabseyoutubetest/MainActivity.java
@@ -19,8 +19,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -45,7 +47,8 @@ public class MainActivity extends AppCompatActivity {
     private Student student;
     private String TAG="MainActivity";
     private DatabaseReference myRef;
-
+    private ListView myListView;
+    private FirebaseListAdapter<Student> mAdapter;
 
 
     @Override
@@ -54,11 +57,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mName = (EditText) findViewById(R.id.mUploadName);
-        mEmail = (EditText) findViewById(R.id.mUploadEmailId);
-        mAge = (EditText) findViewById(R.id.mUploadAge);
+//        mName = (EditText) findViewById(R.id.mUploadName);
+//        mEmail = (EditText) findViewById(R.id.mUploadEmailId);
+//        mAge = (EditText) findViewById(R.id.mUploadAge);
 
 
+        myListView= (ListView) findViewById(R.id.my_lsit_view);
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         //DataBase raf
@@ -67,65 +71,77 @@ public class MainActivity extends AppCompatActivity {
         myRef.keepSynced(true);
 
 
-
-        readFromDataBase = (Button) findViewById(R.id.m_read_button);
-
-        // Write to database
-        mbutton = (Button) findViewById(R.id.m_button);
-        mbutton.setOnClickListener(new View.OnClickListener() {
+        mAdapter = new FirebaseListAdapter<Student>(this, Student.class, android.R.layout.simple_list_item_1, myRef) {
             @Override
-            public void onClick(View v) {
+            protected void populateView(View view, Student data, int position) {
+                ((TextView)view.findViewById(android.R.id.text1)).setText(data.getEmailId());
 
-                // String outout=new Translate(sl, tl, text)
-
-
-                student = new Student(mEmail.getText().toString(), Integer.parseInt(mAge.getText().toString()), mName.getText().toString());
-
-                // Write a message to the database
-
-                myRef.push().setValue(student);
-
-
-                mName.getText().clear();
-                mEmail.getText().clear();
-                mAge.getText().clear();
 
 
             }
-        });
+        };
+        myListView.setAdapter(mAdapter);
 
 
-// ...
-
-        readFromDataBase.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                myRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-
-                        Log.e("Count", "" + dataSnapshot.getChildrenCount());
-                        for (DataSnapshot mydata : dataSnapshot.getChildren()) {
-                            Student s = mydata.getValue(Student.class);
-
-                            Log.e("MY Email", "" + s.getEmailId());
-                            Log.e("MY Name ", "" + s.getName());
-                            Log.e("MY age", "" + s.getAge());
-
-                        }
-
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-
-            }
-        });
-
+//
+//        readFromDataBase = (Button) findViewById(R.id.m_read_button);
+//
+//        // Write to database
+//        mbutton = (Button) findViewById(R.id.m_button);
+//        mbutton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                // String outout=new Translate(sl, tl, text)
+//
+//
+//                student = new Student(mEmail.getText().toString(), Integer.parseInt(mAge.getText().toString()), mName.getText().toString());
+//
+//                // Write a message to the database
+//
+//                myRef.push().setValue(student);
+//
+//
+//                mName.getText().clear();
+//                mEmail.getText().clear();
+//                mAge.getText().clear();
+//
+//
+//            }
+//        });
+//
+//
+//// ...
+//
+//        readFromDataBase.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                myRef.addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//                        Log.e("Count", "" + dataSnapshot.getChildrenCount());
+//                        for (DataSnapshot mydata : dataSnapshot.getChildren()) {
+//                            Student s = mydata.getValue(Student.class);
+//
+//                            Log.e("MY Email", "" + s.getEmailId());
+//                            Log.e("MY Name ", "" + s.getName());
+//                            Log.e("MY age", "" + s.getAge());
+//
+//                        }
+//
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(DatabaseError databaseError) {
+//
+//                    }
+//                });
+//
+//            }
+//        });
+//
 
     }
     @Override
